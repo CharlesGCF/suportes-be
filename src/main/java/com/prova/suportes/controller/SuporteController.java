268b33d9548cd1134e78d00aaf23088a42db1105
Charles Guedes <charlesgcf@gmail.com>
2023-12-10 18:27:02 -0300

    Criação de log

--- src/main/java/com/prova/suportes/controller/SuporteController.java
+++ src/main/java/com/prova/suportes/controller/SuporteController.java
@@ -1,6 +1,7 @@
 package com.prova.suportes.controller;
 
 import com.prova.suportes.service.SuporteService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 @RequestMapping("/v1/validate")
 public class SuporteController {
@@ -16,8 +18,10 @@ public class SuporteController {
 
     @PostMapping
     public ResponseEntity<Void> validaExpressao(@RequestBody String expressao){
+        log.info("Valida Expressao: {}", expressao);
         service.validaExpressao(expressao);
 
+        log.info("Expressao validada com sucesso.");
         return ResponseEntity.ok().build();
     }
 }
--- src/main/java/com/prova/suportes/handler/Handler.java
+++ src/main/java/com/prova/suportes/handler/Handler.java
@@ -1,23 +1,27 @@
 package com.prova.suportes.handler;
 
 import com.prova.suportes.handler.exception.ExpressaoInvalidaException;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 
+@Slf4j
 @ControllerAdvice
 public class Handler {
     @ExceptionHandler(ExpressaoInvalidaException.class)
     public ResponseEntity<Object> handlerExpressaoInvalidaException(ExpressaoInvalidaException ex, WebRequest request) {
         String mensagemErro = ex.getMessage();
+        log.info("handlerExpressaoInvalidaException: {}", ex.getMessage());
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(mensagemErro);
     }
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<Object> handlerException(Exception ex, WebRequest request) {
         String mensagemErro = "Ocorreu um erro interno";
+        log.error("Exception: {}", ex.getMessage());
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(mensagemErro);
     }
 }
